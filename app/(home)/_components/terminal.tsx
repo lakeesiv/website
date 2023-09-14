1fa7ef9f4d9da52d9e1863b4c0b0f256c131e9bb
Lakee Sivaraya <71339072+lakeesiv@users.noreply.github.com>
2023-09-14 14:36:49 +0000

    typed terminal

--- app/(home)/_components/terminal.tsx
+++ app/(home)/_components/terminal.tsx
@@ -1,8 +1,38 @@
-import React from "react";
+"use client";
 
-interface TerminalProps extends React.HTMLAttributes<HTMLDivElement> {}
+import React, { FC, HTMLAttributes, useEffect, useRef } from "react";
+import Typed from "typed.js";
+import siteConfig from "site.config";
 
-const Terminal: React.FC<TerminalProps> = ({}) => {
+interface TerminalProps extends HTMLAttributes<HTMLDivElement> {}
+
+const Terminal: FC<TerminalProps> = ({}) => {
+  const el = useRef(null);
+  const typed = useRef<Typed | null>(null);
+
+  useEffect(() => {
+    const stats = "`" + JSON.stringify(siteConfig.stats, null, 4);
+    +"`";
+    const text = [
+      "`~\\Desktop\\stats>`^1000 ls^1000\n `lakee.py`\n`~\\Desktop\\stats>`^1000 python ^1000 \n `Python 3.8.3`\n `>>>`^1000 <span style='color:#FF1493'>import</span> lakee ^1000 \n `>>>`^1000 <span style='color:#0080FF'>print(</span>lakee<span style='color:#0080FF'>.get_stats()</span><span style='color:#0080FF'>)</span>^1000\n".concat(
+        `${stats}`
+      ),
+    ];
+
+    const options = {
+      strings: text,
+      typeSpeed: 40,
+      backSpeed: 0,
+      loop: false,
+      cursorChar: "_",
+    };
+    typed.current = new Typed(el.current, options);
+
+    return () => {
+      typed?.current?.destroy();
+    };
+  }, []);
+
   return (
     <div className="flex flex-col w-full items-center">
       <div className="w-[80%]  bg-black rounded-t-xl z-10 items-center flex flex-col">
@@ -10,7 +40,11 @@ const Terminal: React.FC<TerminalProps> = ({}) => {
           lakeesiv/website
         </p>
       </div>
-      <div className="w-[80%] h-96 bg-slate-800 rounded-b-xl z-10"></div>
+      <div className="w-[80%]  bg-slate-800 rounded-b-xl z-10">
+        <div className="p-4">
+          <span className="font-mono whitespace-pre" ref={el} />
+        </div>
+      </div>
     </div>
   );
 };
--- site.config.ts
+++ site.config.ts
@@ -1,9 +1,20 @@
 const siteConfig = {
   blogDatabaseId: "a83071d8-1416-44a9-98cf-45638a583a82",
-  eventsDatabaseId: "533a35ca-b69f-4a5d-add8-35c083eaa97e",
-  fundingUrl: "https://youtu.be/dQw4w9WgXcQ",
-  primarySiteUrl: "https://emgineer.vercel.app",
-  admins: ["ls914", "basiltfake", "th621"],
+  stats: {
+    name: "Lakee Sivaraya",
+    occupation: "3rd Year Student",
+    university: "University of Cambridge",
+    subject: "Information and Computer Engineering",
+    skills: [
+      "Javascript/Typescript",
+      "React (Next.js, Astro)",
+      "Vue",
+      "Cloud Computing (AWS, GCP, Firebase)",
+      "Python",
+      "Styling (Chakra, Mantine, Tailwind, Bootstrap)",
+      "git",
+    ],
+  },
 };
 
 export default siteConfig;
