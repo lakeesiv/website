ffa7dd8645090fe68a857bc93c031446796ffad6
Lakee Sivaraya <71339072+lakeesiv@users.noreply.github.com>
2023-09-14 15:41:00 +0000

    about page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 86400;
 export default function Home() {
   return (
     <>
-      <section className="flex flex-col items-center justify-center md:px-12  pb-12 gap-8">
+      <section className="flex flex-col items-center justify-center md:px-12 pb-12 gap-8">
         <Title animate>Lakee Sivaraya</Title>
         <Terminal />
       </section>
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,11 +3,19 @@ import { Title } from "components/text";
 
 const Page = () => {
   return (
-    <div>
+    <section className="px-24 pb-12 gap-8">
       <Title variant="h2" size="md" className="mt-4">
-        Committee
+        About
       </Title>
-    </div>
+      <div className="mt-4">
+        <p>
+          Hi! My name is Lakee (pronounced lack-E), and I am a Third Year
+          Engineering student at the Univeristy of Cambridge. My interests lie
+          in fullstack web development plus a bit of machine learning. I mainly
+          specialise in React, Next.js, and Node.js.
+        </p>
+      </div>
+    </section>
   );
 };
 
