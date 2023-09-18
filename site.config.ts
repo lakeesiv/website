30a99a165e4a13cd302b803bb70df7090be2b433
Lakee Sivaraya <71339072+lakeesiv@users.noreply.github.com>
2023-09-18 13:48:00 +0000

    updated details

--- site.config.ts
+++ site.config.ts
@@ -3,17 +3,15 @@ const siteConfig = {
   projectsDatabaseId: "18e17d83-1956-42de-a93d-9c34fe391502",
   stats: {
     name: "Lakee Sivaraya",
-    occupation: "3rd Year Student",
+    occupation: "4th Year Student",
     university: "University of Cambridge",
     subject: "Information and Computer Engineering",
     skills: [
       "Javascript/Typescript",
       "React (Next.js, Astro)",
-      "Vue",
+      "Styling (Tailwind, Chakra, Bootstrap)",
       "Cloud Computing (AWS, GCP, Firebase)",
       "Python",
-      "Styling (Chakra, Mantine, Tailwind, Bootstrap)",
-      "git",
     ],
   },
 };
