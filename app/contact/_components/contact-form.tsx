686b85c9e7656d4ff474d2b56353b1d98dc9dd11
Lakee Sivaraya <71339072+lakeesiv@users.noreply.github.com>
2023-09-18 14:11:27 +0000

    contact form

---
+++ app/contact/_components/contact-form.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "components/ui/form";
+import { Input } from "components/ui/input";
+import { Button } from "components/ui/button";
+import { Textarea } from "components/ui/textarea";
+import { Card } from "components/ui/card";
+
+const formSchema = z.object({
+  title: z.string().min(3).max(50),
+  message: z.string().min(10).max(500),
+  email: z.string().email(),
+});
+
+export function ContactForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Card className="p-6">
+      <h1 className="text-2xl font-extrabold mb-4 text-center">Contact</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="email@example.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="message"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Message</FormLabel>
+                <FormControl>
+                  <Textarea placeholder="Your message..." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </Card>
+  );
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ContactForm } from "./_components/contact-form";
+
+const ContactPage = () => {
+  return (
+    <section className="px-24 pb-12 gap-8">
+      <ContactForm />
+    </section>
+  );
+};
+
+export default ContactPage;
