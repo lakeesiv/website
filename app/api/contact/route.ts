994cd185b15bb28904ccd85130dcbdab83d65e98
Lakee Sivaraya <71339072+lakeesiv@users.noreply.github.com>
2023-09-20 13:55:40 +0000

    removed unused

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,22 +1,20 @@
 import { contactFormSchema } from "app/contact/_components/contact-form";
 import { NextResponse } from "next/server";
-// import { verifyHcaptchaToken } from "verify-hcaptcha";
+import { verifyHcaptchaToken } from "verify-hcaptcha";
 import siteConfig from "site.config";
-
-export const runtime = "edge";
 
 export const POST = async (request: Request) => {
   const body = request.json();
 
   const { email, message, title, token } = contactFormSchema.parse(body);
 
-  // const result = await verifyHcaptchaToken({
-  //   token: token,
-  //   secretKey: process.env.SECRET_KEY!,
-  //   siteKey: siteConfig.captchaSiteKey!,
-  // });
+  const result = await verifyHcaptchaToken({
+    token: token,
+    secretKey: process.env.SECRET_KEY!,
+    siteKey: siteConfig.captchaSiteKey!,
+  });
 
-  // if (!result.success) {
-  //   return NextResponse.json({ error: "Invalid captcha" }, { status: 400 });
-  // }
+  if (!result.success) {
+    return NextResponse.json({ error: "Invalid captcha" }, { status: 400 });
+  }
 };
